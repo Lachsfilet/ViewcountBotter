bb1ac69fcac67f970963c88b2e6f8e01ba787a23
Lachsfilet <99832883+Lachsfilet@users.noreply.github.com>
2023-12-31 21:51:51 +0000

    added Threading

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 import requests
-import threading
+import multiprocessing
 
-def main():
-    url = input('Please input your Viewcount API URL (for help enter h): ')
-    if url == 'h':
-        print('if you dont know whats going on you probaly dont have a profile readme.md follow the tutorial in the readme.md')
-    requests.get(url)
+def makeRequest(url, iterations, shared_counter):
+    for _ in range(iterations):
+        try:
+            requests.get(url)
+            with shared_counter.get_lock():
+                shared_counter.value += 1
+                print(shared_counter.value)
+        except Exception as e:
+            print(f"Error making request: {e}")
 
 if __name__ == "__main__":
-    main()
+    url = input('Please input your Viewcount API URL (for help enter h): ')
+    total_iterations = int(input('Total iterations eg 100: '))
+    threads = int(input('Threads eg 10: '))
+
+    iterations_per_thread = total_iterations // threads
+    remaining_iterations = total_iterations % threads
+
+    shared_counter = multiprocessing.Value('i', 0)
+    processes = []
+
+    for _ in range(threads):
+        iterations = iterations_per_thread
+        if remaining_iterations > 0:
+            iterations += 1
+            remaining_iterations -= 1
+
+        p = multiprocessing.Process(target=makeRequest, args=(url, iterations, shared_counter))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
